2656818186285b20ad881c95da6558368d26f217
MdRehanAli <rehanali21d@gmail.com>
2026-08-18 21:15:21 +0600

    data types

--- src/premitive.ts
+++ src/premitive.ts
@@ -2,4 +2,19 @@
 // String, number, boolean, undefined, null
 
 // TS Primitive Type: 
+// never, unknown, void 
+
+let userName: string = "Bakervai";
+
+// userName = 12 ; it will be an error because of explicit type string!
+
+let userID = 1234
+
+// userID = "Bakervai"; it will be an error because of implicit type number!
+
+let isAdmin = true;
+
+isAdmin = false; //implicit type of data type
+
+
 
