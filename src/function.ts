baac381f56399d54fd95938cb440d4d03817a814
MdRehanAli <rehanali21d@gmail.com>
2026-08-22 20:41:03 +0600

    Functions & Arrow Functions

--- src/function.ts
+++ src/function.ts
@@ -1,5 +1,16 @@
 // Functions 
 
-function add(num1, num2) {
+// Normal Function and Arrow Function 
+
+function addNormal(num1: number, num2: number): number {
     return num1 + num2;
 }
+
+addNormal(4, 3);
+
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+addArrow(2, 3);
+
+// object => function => method 
+
