9dee9ec920ae4a221cf0fa9027d8c3f37c616239
MdRehanAli <rehanali21d@gmail.com>
2026-08-26 22:25:55 +0600

    Array Destructuring

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -8,11 +8,27 @@ const user = {
     id: 212,
     name: {
         firstName: 'Baker',
-        middleName: 'Vai'
+        lastName: 'Vai',
+        fullName: 'Baker Vai'
     },
     gender: 'Male',
     favoriteColor: 'Black',
 }
 
-const { favoriteColor } = user
-console.log(favoriteColor);
+const { gender } = user
+console.log(gender);
+const { name: {lastName} } = user
+console.log(lastName);
+
+const { favoriteColor: myFavoriteColor } = user  //Naming Alias
+console.log(myFavoriteColor);
+
+const { name: {fullName : myFullName} } = user
+console.log(myFullName);
+
+
+const friends = ['Rahim', 'Karim', 'Jadu', 'Madu', "kadu"]
+
+const [, , myBestFriend]= friends
+
+console.log(myBestFriend)
