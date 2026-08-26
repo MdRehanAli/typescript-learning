a47d274b991bf0325cf97ccf90a3be0222924def
MdRehanAli <rehanali21d@gmail.com>
2026-08-26 21:41:35 +0600

    object Destructuring

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -1,6 +1,18 @@
 //Object Destructuring
 
 
-
-
 //Array Destructuring 
+
+
+const user = {
+    id: 212,
+    name: {
+        firstName: 'Baker',
+        middleName: 'Vai'
+    },
+    gender: 'Male',
+    favoriteColor: 'Black',
+}
+
+const { favoriteColor } = user
+console.log(favoriteColor);
