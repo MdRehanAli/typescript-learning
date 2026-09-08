0ea2a45b57f6f06e364aa7148c3e4a832e57d14d
MdRehanAli <rehanali21d@gmail.com>
2026-09-08 20:50:26 +0600

    questionMark

--- src/questionMark.ts
+++ src/questionMark.ts
@@ -1,4 +1,52 @@
 // ?: ternary operator: decision making
-// ??: nullish coalescing operator
+// ??: nullish coalescing operator: null/undefined 
 // ?. optional chaining 
 
+
+const biyerJonnoEligible = (age: number) => {
+    // if (age >= 21) {
+    //     console.log("You are eligible");
+    // }
+    // else {
+    //     console.log("You are not eligible");
+    // }
+
+
+    const result = age >= 21 ? "You are eligible" : "You are not eligible"
+    console.log(result)
+};
+
+biyerJonnoEligible(21);
+
+
+const userTheme = "Dark theme"; //null or undefined 
+
+const selectedTheme = userTheme ?? 'Light theme'
+
+console.log(selectedTheme)
+
+
+const isAuthenticated = ''
+
+const resultWithTernary = isAuthenticated ? isAuthenticated : "You are guest!"
+const resultWithNullish = isAuthenticated ?? "You are guest!"
+
+console.log({ resultWithTernary }, { resultWithNullish });
+
+
+// Optional Chaining 
+const user: {
+    address: {
+        city: string;
+        town: string;
+        postalCode?: string
+    }
+} = {
+    address: {
+        city: 'Dhaka',
+        town: 'Uttara',
+    }
+}
+
+const postalCode = user?.address?.postalCode
+console.log(postalCode)
