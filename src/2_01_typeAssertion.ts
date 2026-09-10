91bf7f1ad5a87595116bc01357292e3f6ed34c75
MdRehanAli <rehanali21d@gmail.com>
2026-09-10 19:54:05 +0600

    Type Assertion

---
+++ src/2_01_typeAssertion.ts
@@ -0,0 +1,38 @@
+// Type Assertion 
+
+let anything: any;
+
+anything = "Baker";
+
+// (anything as string).endsWith
+
+
+const kgToGramConverter = (input: string | number): string | number | undefined => {
+    if (typeof input === "number") {
+        return input * 1000
+    }
+    else if (typeof input === 'string') {
+        const [value] = input.split(" ")
+        return `Converted output is: ${Number(value) * 1000}`
+    }
+}
+
+const result1 = kgToGramConverter(2) as number;
+console.log({ result1 })
+const result2 = kgToGramConverter("2 KG") as string;
+console.log({ result2 })
+
+
+
+
+type CustomError = {
+    message: string;
+}
+
+try {
+
+}
+
+catch (err) {
+    console.log((err as CustomError).message);
+}
